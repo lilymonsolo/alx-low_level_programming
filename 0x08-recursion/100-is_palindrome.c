b378c86114495e63fe1b4824ff9500c9bba7d34e
lilymonsolo <101515201+lilymonsolo@users.noreply.github.com>
2022-04-27 23:03:38 +0300

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,68 @@
-#include "holberton.h"
+#include "main.h"
 
-int check_palindrome(char *s);
+int _strlen_recursion(char *s);
+int _comparission(char *s, int legth);
 
 /**
-  * is_palindrome - Returns if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
+ * is_palindrome - function to know when a string is palindrome or not..
+ *
+ * @s: is a pointer to a string.
+ *
+ * Return: Returns _comparission parameters.
+ *
+**/
+
 int is_palindrome(char *s)
 {
-	if (*s == '0')
-		return (1);
 
-	return (check_palindrome(s));
-}
+int len = _strlen_recursion(s);
 
-/**
-  * check_palindrome - Check if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
-	{
-		s++;
-		l--;
-	}
-	else
-	{
-		return (0);
-	}
-
+if (len == 1)
 	return (1);
+else if (len < 0)
+	return (0);
+else
+	return (_comparission(s, len));
+
 }
 
 /**
-  * _strlen_recursion - Get the length of a string
-  * @s: the string to get the length
-  *
-  * Return: the string length
-  */
+ * _strlen_recursion - Function for counting size of a string.
+ *
+ * @s: points to the string.
+ *
+ * Return: Returns the size of the string.
+ *
+**/
+
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
 
-	s++;
-	return (_strlen_recursion(s) + 1);
+if (*s == 0)
+	return (0);
+
+return (1 + _strlen_recursion(s + 1));
+
+}
+
+/**
+ * _comparission - Function that compares initial index and last index.
+ *
+ * @s: points to the string.
+ * @length: is the size of the string.
+ *
+ * Return: Returns increased s and decreased length
+ *
+**/
+
+int _comparission(char *s, int length)
+{
+
+if (length <= 1)
+	return (1);
+else if (*s == s[length - 1])
+	return (_comparission(++s, ((length - 1) - 1)));
+else
+	return (0);
+
 }
