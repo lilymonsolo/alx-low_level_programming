da376ba23a99b5777f4f3e74047ce087f446ebcb
lilymonsolo <101515201+lilymonsolo@users.noreply.github.com>
2022-05-05 16:06:37 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include "holberton.h"
 /**
  * strncat_mod - concatenates string with n bytes from another string
  * @dest: destination string
